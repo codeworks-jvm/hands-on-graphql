7796c32b2e732971e2d1349f58b8c7e27b257981
BANGALY KAKE <bangaly.kake@gmail.com>
2020-11-06 15:26:29 +0100

    Mount express-graphql as a route handler

--- graphql-with-nodejs/server.js
+++ graphql-with-nodejs/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const { graphqlHTTP } = require('express-graphql');
 const server = express();
+
+server.use(
+    '/codeWorksGraphQL',
+    graphqlHTTP({
+    graphiql:true
+}));
 
 server.listen(8080, () => {
     console.log('Server is listening on port 8080');
